fac70b6d0bb04fb3b757779daf88c45d2467271c
Christopher Speltz <chrisspeltz@msn.com>
2022-03-06 19:54:17 -0600

    21.5.4 create and implement authentication functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import NoMatch from "./pages/NoMatch";
@@ -19,6 +21,16 @@ import Home from "./pages/Home";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
