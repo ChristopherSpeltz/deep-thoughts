f3285fb72fdbfbeaca32851d52b429f6e23fc0b4
Christopher Speltz <chrisspeltz@msn.com>
2022-03-08 22:13:24 -0600

    21.6.4 create a thought form component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,8 @@ import { ADD_FRIEND } from "../utils/mutations";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import FriendList from "../components/FriendList";
+import ThoughtForm from "../components/ThoughtForm";
+
 import Auth from "../utils/auth";
 
 const Profile = () => {
@@ -75,6 +77,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
